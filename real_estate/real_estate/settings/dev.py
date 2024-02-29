6744644ebb279d100ca0cf8c6b1991de1344e403
markdunn72 <markdunn2012@gmail.com>
2024-02-29 15:59:44 +0000

    feat: user auth - register, login, password reset

--- real_estate/real_estate/settings/dev.py
+++ real_estate/real_estate/settings/dev.py
@@ -1,1 +1,18 @@
 from real_estate.settings.base import *
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# # EMAIL_HOST = env("EMAIL_HOST")
+# # EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+# # EMAIL_PORT = env("EMAIL_PORT")
+# # EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+#
+# EMAIL_HOST = "sandbox.smtp.mailtrap.io"
+# EMAIL_HOST_USER = "e6a2e6f8defaf9"
+# EMAIL_HOST_PASSWORD = "5cc7dfb3b41583"
+# EMAIL_PORT = "2525"
+
+DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
